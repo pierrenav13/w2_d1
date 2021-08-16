3ec4730b508d71f992127a8e447908993d74f2c6
pierre <pnavarin@gmail.com>
2021-08-16 12:05:54 -0400

    Completed getters for bootcamp_project

--- bootcamp_project/lib/bootcamp.rb
+++ bootcamp_project/lib/bootcamp.rb
@@ -1,3 +1,27 @@
 class Bootcamp
-  
+    def initialize(name, slogan, student_capacity)
+        @name = name
+        @slogan = slogan
+        @student_capacity = student_capacity
+        @teachers = []
+        @students = []
+        @grades = Hash.new {|h,k| h[k] = []}
+    end
+    
+    def name
+        @name
+    end
+
+    def slogan
+        @slogan
+    end
+
+    def teachers
+        @teachers
+    end
+
+    def students
+        @students
+    end
+    
 end
